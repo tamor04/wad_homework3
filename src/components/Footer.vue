bf6dd9f2f04661bdf7352aa4c4a43c03aae4bcc1
tamor04 <tamor.tomson@gmail.com>
2024-11-24 20:01:08 +0200

    põhi

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <p>Footer</p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+<!--Include footer-specific styles here -->
+<style scoped src="@assets/footer_style.css"></style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Index from './Index.vue'
+import Login from './Login.vue'
+
 
 createApp(App).mount('#app')
